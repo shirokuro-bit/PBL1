061c57cc2f7c4735231b82f5f0aadc249b9a8032
kazusan-baba <Desktop>
2023-10-24 04:13:30 +0900

    モーダル関連のリファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {animateScroll as scroll, Link} from "react-scroll";
-import Modal from 'react-modal';
 import './App.css';
 
 import styles from './App.module.css';
@@ -9,17 +8,13 @@ import HorizontalRule from "./components/hr";
 import ModalContent from './components/modal';
 
 function App() {
-  const [modalIsOpen1, setIsOpen1] = React.useState(false);
-  const [modalIsOpen2, setIsOpen2] = React.useState(false);
-  const [modalIsOpen3, setIsOpen3] = React.useState(false);
-  const [modalIsOpen4, setIsOpen4] = React.useState(false);
-  
-  const scrollToTop = () => {
-    scroll.scrollToTop();
-  }
+  const [modalIsOpen1, setIsOpen1] = useState(false);
+  const [modalIsOpen2, setIsOpen2] = useState(false);
+  const [modalIsOpen3, setIsOpen3] = useState(false);
+  const [modalIsOpen4, setIsOpen4] = useState(false);
   
   return (
-    <div className="App" style={{background: 'url(' + `${process.env.PUBLIC_URL}/BackgroundImg.jpg` + ') no-repeat top / cover'}}>
+    <div className="App" style={{background: `url(${process.env.PUBLIC_URL}/BackgroundImg.jpg) no-repeat top / cover`}}>
         <Header/>
         <HorizontalRule/>
         <main>
@@ -45,13 +40,11 @@ function App() {
                 <p className={styles.text}>
                   農業は高齢化が顕著に進み、新規の若い農家が減少している。
                 </p>
-                <img src={"https://d1obh0a64dzipo.cloudfront.net/images/5856.jpg"} onClick={() => setIsOpen1(true)}/>
-                <Modal isOpen={modalIsOpen1} onRequestClose={() => setIsOpen1(false)}>
-                  <div className={styles.modal}>
-                    <img src={"https://d1obh0a64dzipo.cloudfront.net/images/5856.jpg"} width={"65%"}/>
-                    <a href={"https://minorasu.basf.co.jp/80466"}><p>出典：農林水産省「農林業センサス」よりminorasu編集部作成</p></a>
-                  </div>
-                </Modal>
+                <ModalContent image={"https://d1obh0a64dzipo.cloudfront.net/images/5856.jpg"}
+                              modalIsOpen={modalIsOpen1}
+                              setIsOpen={setIsOpen1}>
+                  <a href={"https://minorasu.basf.co.jp/80466"}><p>出典：農林水産省「農林業センサス」よりminorasu編集部作成</p></a>
+                </ModalContent>
               </div>
               <div className={styles.box1}>
                 <p className={styles.text}>
@@ -66,14 +59,11 @@ function App() {
                   {/*切っ掛けの為になればいいと、このようなプロジェクトを考えています。<br/>*/}
                   見方が変わるきっかけに！
                 </p>
-  
-                <img src={"https://www.ryutsu-kenkyusho.co.jp/cms/wp-content/uploads/2015/10/93F182CCE7DD82AA8Ea82E991E61848D86_907D87C.png"} onClick={() => setIsOpen2(true)}/>
-                <Modal isOpen={modalIsOpen2} onRequestClose={() => setIsOpen2(false)}>
-                  <div className={styles.modal}>
-                    <img src={"https://www.ryutsu-kenkyusho.co.jp/cms/wp-content/uploads/2015/10/93F182CCE7DD82AA8Ea82E991E61848D86_907D87C.png"} width={"75%"}/>
-                    <a href={"https://www.ryutsu-kenkyusho.co.jp/columns/%E6%96%B0%E8%A6%8F%E5%B0%B1%E8%BE%B2%E3%81%AE%E8%AA%B2%E9%A1%8C%E3%81%A8%E5%B1%95%E6%9C%9B-%EF%BD%9E%E9%A0%91%E5%BC%B5%E3%82%8C%EF%BC%81%E6%96%B0%E8%A6%8F%E5%B0%B1%E8%BE%B2%E8%80%85%EF%BD%9E/"}><p>出典：全国農業会議所「新規就農者の就農実態に関する調査結果」ｎ=711</p></a>
-                  </div>
-                </Modal>
+                <ModalContent image={"https://www.ryutsu-kenkyusho.co.jp/cms/wp-content/uploads/2015/10/93F182CCE7DD82AA8Ea82E991E61848D86_907D87C.png"}
+                              modalIsOpen={modalIsOpen2}
+                              setIsOpen={setIsOpen2}>
+                  <a href={"https://www.ryutsu-kenkyusho.co.jp/columns/新規就農の課題と展望-～頑張れ！新規就農者～/"}><p>出典：全国農業会議所「新規就農者の就農実態に関する調査結果」ｎ=711</p></a>
+                </ModalContent>
               </div>
             </div>
           
@@ -96,26 +86,22 @@ function App() {
               <p className={styles.text}>
                 ・小中学校等の学校教育で農業を体験する機会を作り、農業に興味・関心を持ってもらう。<br/>
               </p>
-              <img src={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/img/einou1.jpg"} onClick={() => setIsOpen3(true)}/>
-              <Modal isOpen={modalIsOpen3} onRequestClose={() => setIsOpen3(false)}>
-                <div className={styles.modal}>
-                  <img src={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/img/einou1.jpg"} width={"75%"}/>
-                  <a href={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/index.html"}><p>出典：農林水産省</p></a>
-                </div>
-              </Modal>
+              <ModalContent image={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/img/einou1.jpg"}
+                            modalIsOpen={modalIsOpen3}
+                            setIsOpen={setIsOpen3}>
+                <a href={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/index.html"}><p>出典：農林水産省</p></a>
+              </ModalContent>
             </div>
             
             <div className={styles.box3}>
               <p className={styles.text}>
                 ・都心部などで農業体験が難しい場合、動画や資料などで農業従事者からノウハウを聞き、同様の体験を行う。
               </p>
-              <img src={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/img/jyugyou2.jpg"} onClick={() => setIsOpen4(true)}/>
-              <Modal isOpen={modalIsOpen4} onRequestClose={() => setIsOpen4(false)}>
-                <div className={styles.modal}>
-                  <img src={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/img/jyugyou2.jpg"} width={"75%"}/>
-                  <a href={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/index.html"}><p>出典：農林水産省</p></a>
-                </div>
-              </Modal>
+              <ModalContent image={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/img/jyugyou2.jpg"}
+                            modalIsOpen={modalIsOpen4}
+                            setIsOpen={setIsOpen4}>
+                <a href={"https://www.maff.go.jp/j/nousin/sekkei/midori/m_manabi/taiken/index.html"}><p>出典：農林水産省</p></a>
+              </ModalContent>
             </div>
       
           </div>
@@ -130,7 +116,7 @@ function App() {
               {/*<img alt={"かっこいい画像"}/>*/}
             </div>
           </div>
-          <span className={styles.pageTop} onClick={() => scrollToTop()}>
+          <span className={styles.pageTop} onClick={scroll.scrollToTop}>
             <i className={"material-symbols-outlined"}>arrow_upward</i>
           </span>
         </main>
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Modal from 'react-modal';
+import styles from '../App.module.css';
 
-const ModalContent = (link:string, modalIsOpen: boolean, setIsOpen: React.Dispatch<React.SetStateAction<boolean>>): JSX.Element => {
+type ModalContentType = {
+  children: JSX.Element;
+  image: string,
+  modalIsOpen: boolean,
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ModalContent = (props: ModalContentType): JSX.Element => {
   return (
-    <div>
-      <img src={link} onClick={() => setIsOpen(true)}/>
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setIsOpen(false)}>
-        <img src={link}/>
+    <>
+      <img src={props.image} onClick={() => props.setIsOpen(true)}/>
+      <Modal isOpen={props.modalIsOpen} onRequestClose={() => props.setIsOpen(false)}>
+        <div className={styles.modal}>
+          <img src={props.image}/>
+          {props.children}
+        </div>
       </Modal>
-    </div>
+    </>
   );
 };
 
